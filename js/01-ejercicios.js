8e61f85fbb6f95c27ea00ab02c3534621a169a9e
Cristian Diaz <diazcristian0210@gmail.com>
2023-12-21 11:20:09 -0300

    Agregue una suma, resta y un nombre completo

---
+++ js/01-ejercicios.js
@@ -0,0 +1,14 @@
+let impar = 15;
+let par = 30;
+
+document.write("El resultado de la suma es " + (impar + par));
+document.write("<br> El resultado de la resta es " + (par - impar));
+
+document.write (" <br> El resultado de la multiplicacion es " + (par * impar));
+document.write (" <br> El resultado de la division es " + (par / impar));
+
+let nombre = "Cristian"
+let apellido = "Diaz"
+let nombrecompleto = "Cristian Diaz"
+
+document.write (" <br> El nombre completo es " + (nombrecompleto))
