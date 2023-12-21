c8a0d88b750b74bae280835dfbb9f9f0889f6a3b
Cristian Diaz <diazcristian0210@gmail.com>
2023-12-21 11:56:33 -0300

    Agregue un conversor de grados celsius a farenheit

--- js/01-ejercicios.js
+++ js/01-ejercicios.js
@@ -11,4 +11,13 @@ let nombre = "Cristian"
 let apellido = "Diaz"
 let nombrecompleto = "Cristian Diaz"
 
-document.write (" <br> El nombre completo es " + (nombrecompleto))
+document.write (" <br> El nombre completo es " + (nombrecompleto));
+
+let gradosCelsius = 35;
+let gradosFarenheit = (gradosCelsius * 9) / 5 + 32;
+
+document.write(
+    "<br>" +
+    gradosCelsius + " °C grados celsius son equivalentes a " +
+    gradosFarenheit + " °F grados farenheit"
+)
