7ebb1df9045272e4f675c537b98dfc787b6b3fbc
Cristian Diaz <diazcristian0210@gmail.com>
2024-02-05 21:23:54 -0300

    Agregamos metodos para eliminar elementos del array, shift,pop,splice

--- js/055-arrays.js
+++ js/055-arrays.js
@@ -4,7 +4,7 @@ const listaProductos = [];
 
 // crear un array con datos siempre usar una S al final, que de a entender que hay varias listas separados por coma
 
-const juegos = ['Stardew valley','fifa', 'league of legends', true, 2024, 'fornite',];
+const juegos = ['Stardew valley','fifa', 1, 'league of legends', true, 2024, 'fornite',];
 // mostrar un array
 console.log(listaProductos);
 document.write(juegos);
@@ -58,3 +58,51 @@ document.write(`<li>${juegos[posicion]}</li>`)
 }
 document.write(`</ul>`)
 
+
+//modificar elementos del array
+
+juegos[3] = 'Dont starve'
+// juegos = 'Dont starve' Este codigo es incorrecto no puedo cambiar el contenido total del array por otro valor;
+
+document.write(`<h2>Modificamos el juego de la posicion 2 array🎮(${juegos.length})</h2>`)
+
+document.write(`<ul>`)
+for(let posicion=0; posicion < juegos.length; posicion++){
+document.write(`<li>${juegos[posicion]}</li>`)
+}
+document.write(`</ul>`)
+
+// Accion para eliminar elementos del array
+juegos.shift();
+document.write(`<h2>Borramos el primer elemento del array🎮(${juegos.length})</h2>`)
+
+document.write(`<ul>`)
+for(let posicion=0; posicion < juegos.length; posicion++){
+document.write(`<li>${juegos[posicion]}</li>`)
+}
+document.write(`</ul>`)
+
+//Accion para eliminar elementos del medio del array
+
+juegos.splice(3,1)
+// juegos.splice(3,2) // borramos desde la posicion 3, la cantidad de 2 elementos
+// juegos.splice(3) borramos desde la posicion 3 el resto de los elemtnos del array
+document.write(`<h2>Borramos el elemento de la posicion 3 del array🎮(${juegos.length})</h2>`)
+
+document.write(`<ul>`)
+for(let posicion=0; posicion < juegos.length; posicion++){
+document.write(`<li>${juegos[posicion]}</li>`)
+}
+document.write(`</ul>`)
+
+
+// eliminar el  ultimo elemento es pop LOS ARRAY SIEMPRE CON ()
+
+juegos.pop();
+document.write(`<h2>Borramos el ultimo elemento del array🎮(${juegos.length})</h2>`)
+
+document.write(`<ul>`)
+for(let posicion=0; posicion < juegos.length; posicion++){
+document.write(`<li>${juegos[posicion]}</li>`)
+}
+document.write(`</ul>`)
